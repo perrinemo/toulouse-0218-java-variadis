a068bb5ba826b78edbc9d1b7e1e9fd6c5ec46906
Perrine <morandperrine@hotmail.fr>
2018-03-30 11:09:28 +0200

    Action sur l'avatar + photo ronde

--- app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
+++ app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
@@ -2,6 +2,10 @@ package fr.wildcodeschool.variadis;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputType;
@@ -11,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    ImageView avatar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +31,27 @@ public class ProfilActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        avatar = findViewById(R.id.avatar);
+        avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+
+        RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(
+                ProfilActivity.this.getResources(), bitmap);
+
+        roundedBitmapDrawable.setCircular(true);
+        avatar.setImageDrawable(roundedBitmapDrawable);
     }
 }
