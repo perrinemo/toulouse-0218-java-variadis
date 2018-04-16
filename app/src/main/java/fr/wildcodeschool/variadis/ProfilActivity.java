96d022033eb56a7fca6c791961f705c09b440152
Perrine <morandperrine@hotmail.fr>
2018-04-16 14:02:32 +0200

    modif pseudo textview en edittext

--- app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
+++ app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
@@ -7,6 +7,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -25,7 +26,7 @@ public class ProfilActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
-        TextView changePseudo = findViewById(R.id.name_pseudo);
+        EditText changePseudo = findViewById(R.id.name_pseudo);
         ImageView ivHerbier = findViewById(R.id.img_herbier);
         ImageView ivMap = findViewById(R.id.img_map);
         ImageButton deco = findViewById(R.id.btn_logout);
