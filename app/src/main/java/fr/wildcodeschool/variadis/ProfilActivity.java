d79c7a36cec9fd91f909c3b7358e62532eaf7a03
Perrine <morandperrine@hotmail.fr>
2018-04-16 14:19:11 +0200

    Ajout de ProfilModel et de ProfilHelper

--- app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
+++ app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
@@ -6,33 +6,40 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.storage.StorageReference;
 
 
 public class ProfilActivity extends AppCompatActivity {
 
-    ImageView avatar;
+    private ImageView mAvatar;
+    private EditText mEditPseudo = findViewById(R.id.edit_pseudo);
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+    private String uid;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
 
-        EditText changePseudo = findViewById(R.id.name_pseudo);
+        mEditPseudo = findViewById(R.id.edit_pseudo);
         ImageView ivHerbier = findViewById(R.id.img_herbier);
         ImageView ivMap = findViewById(R.id.img_map);
         ImageButton deco = findViewById(R.id.btn_logout);
         final FirebaseAuth auth = FirebaseAuth.getInstance();
 
-        avatar = findViewById(R.id.avatar);
+        mAvatar = findViewById(R.id.avatar);
+
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         deco.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,7 +60,7 @@ public class ProfilActivity extends AppCompatActivity {
             }
         });
 
-        avatar.setOnClickListener(new View.OnClickListener() {
+        mAvatar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -75,6 +82,6 @@ public class ProfilActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
-        Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(avatar);
+        Glide.with(this).load(bitmap).apply(RequestOptions.circleCropTransform()).into(mAvatar);
     }
 }
