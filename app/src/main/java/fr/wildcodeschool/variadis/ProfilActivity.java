36261d497f0e48cb48159873228b5ccc37dd1dea
Perrine <morandperrine@hotmail.fr>
2018-04-19 09:51:47 +0200

    Ajout bouton info dans le profil + action dessus

--- app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
+++ app/src/main/java/fr/wildcodeschool/variadis/ProfilActivity.java
@@ -41,6 +41,7 @@ public class ProfilActivity extends AppCompatActivity {
         ImageView ivHerbier = findViewById(R.id.img_herbier);
         ImageView ivMap = findViewById(R.id.img_map);
         ImageButton deco = findViewById(R.id.btn_logout);
+        ImageView info = findViewById(R.id.btn_info);
         Button okPseudo = findViewById(R.id.btn_ok_pseudo);
         final FirebaseAuth auth = FirebaseAuth.getInstance();
         FirebaseStorage firebaseStorage = FirebaseStorage.getInstance();
@@ -84,6 +85,14 @@ public class ProfilActivity extends AppCompatActivity {
                 startActivity(intent);
                 auth.signOut();
                 finish();
+            }
+        });
+
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProfilActivity.this, InfosActivity.class);
+                startActivity(intent);
             }
         });
 
