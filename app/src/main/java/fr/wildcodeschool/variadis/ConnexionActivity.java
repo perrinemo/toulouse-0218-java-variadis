fded59b9545ddea0ae0cb1c35381c0cdd9e4f2de
Perrine <morandperrine@hotmail.fr>
2018-05-03 09:35:25 +0200

    nettoyage du code

--- app/src/main/java/fr/wildcodeschool/variadis/ConnexionActivity.java
+++ app/src/main/java/fr/wildcodeschool/variadis/ConnexionActivity.java
@@ -13,19 +13,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import static fr.wildcodeschool.variadis.MapsActivity.sBackPress;
 
 /**
  * Created by perrine on 16/04/18.
@@ -109,6 +100,7 @@ public class ConnexionActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     public void onBackPressed() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
